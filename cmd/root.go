77be3df35253c507f76decf97064ddca9dcad678
chrismaher <chrismaher37@gmail.com>
2020-12-18 18:30:01 -0500

    improve viper usage in root.go
    * add secret and token flags

--- cmd/root.go
+++ cmd/root.go
@@ -14,10 +14,11 @@ import (
 )
 
 var (
-	cfgFile  string
-	dataFile string
-	service  google.Client
-	connect  redshift.Connection
+	configFile string
+	secretFile string
+	tokenFile  string
+	service    google.Client
+	connect    redshift.Connection
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -40,32 +41,33 @@ func init() {
 	cobra.OnInitialize(initConfig)
 	cobra.OnInitialize()
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.redsheets/config.toml)")
-	rootCmd.PersistentFlags().StringVar(&dataFile, "datastore", "", "")
+	rootCmd.PersistentFlags().StringVar(&configFile, "config", "", "config file (default is $HOME/.redsheets/config.toml)")
+	rootCmd.PersistentFlags().StringVar(&secretFile, "secret", "", "")
+	rootCmd.PersistentFlags().StringVar(&tokenFile, "token", "", "")
 }
 
-// initConfig reads in config file and ENV variables if set.
+// initConfig reads in config file and ENV variables if set
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		path, err := homedir.FullPath(".redsheets")
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	// Find application directory; need to test if it exists?
+	path, err := homedir.FullPath(".redsheets")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-		// Search config in home directory with name ".redsheets" (without extension).
+	if configFile != "" {
+		// Use config file from the flag
+		viper.SetConfigFile(configFile)
+	} else {
+		// Search for config in ~/.redsheets directory
 		viper.AddConfigPath(path)
 		viper.SetConfigName("config.toml")
 		viper.SetConfigType("toml")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	viper.AutomaticEnv() // read environment variables that match
 
-	// If a config file is found, read it in.
+	// If a config file is found, read it
 	if err := viper.ReadInConfig(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -76,13 +78,31 @@ func initConfig() {
 		os.Exit(1)
 	}
 
-	datastore = json.DataStore{Path: "/Users/cmaher/.redsheets/data/redsheets.json"}
-	datastore.Read()
-
-	path, err := homedir.FullPath(viper.GetString("client.secret"))
+	storePath, err := homedir.FullPath(".redsheets/data/redsheets.json")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	service = google.Client{SecretFile: path, TokenFile: "/Users/cmaher/.redsheets/google/token.json"}
+	viper.SetDefault("datastore", storePath)
+
+	if secretFile == "" {
+		secretFile, err = homedir.FullPath(".redsheets", viper.GetString("client.secret"))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	if tokenFile == "" {
+		tokenFile, err = homedir.FullPath(".redsheets", viper.GetString("client.token"))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	datastore = json.DataStore{Path: viper.GetString("datastore")}
+	datastore.Read()
+
+	service = google.Client{SecretFile: secretFile, TokenFile: tokenFile}
 }
